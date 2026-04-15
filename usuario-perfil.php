ab601854da22e74ad29d4c742ebee1c91f0c51c6
Ricardo <ric_rps@hotmail.com>
2026-04-15 06:36:27 -0300

    ajuste

--- usuario-perfil.php
+++ usuario-perfil.php
@@ -12,8 +12,12 @@ $usuario = $userController->buscar();
 
 <head>
     <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <link rel="stylesheet" href="css/styles.css" type="text/css" />
     <title>Editar Perfil</title>
 </head>
 
